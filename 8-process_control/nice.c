1db503c8ac87d765f3032f6fd21e662b7fbc68eb
ubuntu <ubuntu@ubuntu.(none)>
2016-05-31 00:04:36 +0800

    add file nice.c

--- 8-process_control/nice.c
+++ 8-process_control/nice.c
@@ -10,6 +10,63 @@
 #include <sys/param.h>
 #endif
 
-unsigned long long count;
+unsigned long long count=0;
 struct timeval end;
 
+void checktime(char *str){
+	struct timeval tv;
+
+	gettimeofday(&tv,NULL);
+	if(tv.tv_sec >= end.tv_sec && tv.tv_usec >= end.tv_usec){
+		printf("%s count = %lld\n",str,count);
+		exit(0);
+	}
+}
+
+int main(int argc,char *argv[]){
+
+	pid_t pid;
+	char *s;
+	int nzero,ret;
+	int adj = 0;
+
+	setbuf(stdout,NULL);
+#if defined(NZERO)
+	nzero = NZERO;
+#elif defined(_SC_NERO)
+	nzero = sysconf(_SC_NZERO);
+#else
+//#error NZERO undefined
+#endif
+	printf("NZERO = %d\n",nzero);
+	if(argc == 2)
+		adj = strtol(argv[1],NULL,10);
+	gettimeofday(&end,NULL);
+	end.tv_sec += 10;
+
+	if((pid = fork()) < 0){
+		perror("fork");
+	}else if(pid == 0){
+		s = "child";
+		printf("current nice value in child is %d ,adjusting by %d\n",nice(0)+nzero,adj);
+
+		errno = 0;
+		if((ret = nice(adj)) == -1 && errno != 0){
+			perror("nice");
+		}
+
+		printf("now child nice value is %d\n",ret+nzero);
+	}else{
+		s = "parent";
+		printf("curent nice value in parent is %d\n",nice(0)+nzero);
+	}
+	
+	for(;;){
+		if(++count == 0){
+			perror("count");
+		}
+		checktime(s);
+	}	
+
+	exit(0);
+}
